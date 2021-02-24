f7caedda2f299a548334e37f6fabd1ea4810358f
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-02-24 17:21:07 +0530

    crossAxisAlignments Properties

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,36 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-          backgroundColor: Colors.redAccent,
-          body: SafeArea(
-            child: Container(
+          backgroundColor: Colors.teal,
+          body: Column(
+            //verticalDirection: VerticalDirection.down, //to change the dir of column
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly, //to provide space b/w the columns
+            crossAxisAlignment: CrossAxisAlignment.stretch, //align to specific side of the column or can stretch to parent.
+            children: [
+              Container(
+                height: 100.0,
+                //width: 100.0, //used crossAxisAlignment property stretch
+                //margin: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 30.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.lightGreenAccent,
+                child: Text('Container 1'),
 
-            ),
-          ),
+              ),
+              Container(
+                //width: 100,
+                height: 100,
+                padding: EdgeInsets.all(10),
+                child: Text('Container2'),
+                color: Colors.redAccent,),
+              Container(
+                height: 100,
+                //width: 100,
+                padding: EdgeInsets.all(10.0),
+                child: Text('container3'),
+                color: Colors.deepPurple,
+              ),
+            ],
+          )
 
           ),
 
