c07f22186c02d8ed554ce573d81ea4cff0b888be
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-02-24 16:18:35 +0530

    change container widget content

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-          backgroundColor: Colors.greenAccent,
-          body: Container(
-                height: 100.0,
-                width: 100.0,
-                margin: EdgeInsets.fromLTRB(30.0, 30.0, 50.0, 30.0),
-                padding: EdgeInsets.all(20.0),
-                color: Colors.lightGreenAccent,
-                child: Text('Hello Flutter World'),
+          backgroundColor: Colors.redAccent,
+          body: SafeArea(
+            child: Container(
 
-              ),
+            ),
+          ),
 
           ),
 
