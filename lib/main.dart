aaf7c2addc0af8b869c9bbb40f3c2798eb33cd3a
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-03-06 11:33:16 +0530

    font & image

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() =>  runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,38 +11,36 @@ class MyApp extends StatelessWidget {
 
       home: Scaffold(
           backgroundColor: Colors.teal,
-          body: Column(
-            //verticalDirection: VerticalDirection.down, //to change the dir of column
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly, //to provide space b/w the columns
-            crossAxisAlignment: CrossAxisAlignment.stretch, //align to specific side of the column or can stretch to parent.
-            children: [
-              Container(
-                height: 100.0,
-                //width: 100.0, //used crossAxisAlignment property stretch
-                //margin: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 30.0),
-                padding: EdgeInsets.all(10.0),
-                color: Colors.lightGreenAccent,
-                child: Text('Container 1'),
-
-              ),
-              Container(
-                //width: 100,
-                height: 100,
-                padding: EdgeInsets.all(10),
-                child: Text('Container2'),
-                color: Colors.redAccent,),
-              Container(
-                height: 100,
-                //width: 100,
-                padding: EdgeInsets.all(10.0),
-                child: Text('container3'),
-                color: Colors.deepPurple,
-              ),
-            ],
-          )
-
+          //body: Column(
+            body: SafeArea(child: Column( children: <Widget> [
+          Center(
+            child: CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/srone.jpeg'),
+                ),
           ),
+              Text(
+                'Srone Codes',
+                style: TextStyle(
+                  fontSize: 40.5,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'
+                ),
+              ),
+              Text(
+                'FUTURE DEVELOPER',
+                style:  TextStyle(
+                  fontSize: 20.5,
+                  color: Colors.white,
+                  fontFamily: 'SourceSansPro-Regular'
+                ),
+              )
 
+          ],
+        ),
+       ),
+      ),
     );
   }
 }
