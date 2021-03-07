3e4cbd4af71de02fc6a79b59ddbb2697a1e87b2b
Saransh Chauhan <saranshchauhan700@gmail.com>
2021-03-07 11:58:24 +0530

    added ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.deepOrangeAccent,
           //body: Column(
-            body: SafeArea(child: Column( children: <Widget> [
+            body: SafeArea(child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
           Center(
             child: CircleAvatar(
                 radius: 50.0,
@@ -37,72 +39,70 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'SourceSansPro-Regular'
                 ),
               ),
+              SizedBox(
+                height:  20.0,
+                width: 200,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 5,
+                  indent: 2.5,
+                  endIndent: 2.5,
+                ),
+              ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal:  50.0),
                 //padding: EdgeInsets.all(10.0),
                 child:  Padding( // padding class
                   padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
+                  child: ListTile(
+                      leading: Icon(
                         Icons.phone,
                         size: 30.0,
                         color: Colors.teal.shade900,
                       ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
+                      title: Text(
                         '+91 12345 67890',
                         style: TextStyle(
                           color: Colors.teal.shade900,
-                          fontSize: 30.0,
+                          fontSize: 24.5,
                           fontFamily: 'SourceSansPro-Bold',
                           fontStyle: FontStyle.italic,
 
                         ),
                       ),
 
-                    ],
+                  ),
                   ),
                 ),
-              ),
+
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal:  50.0),
                 //padding: EdgeInsets.all(10.0), because card doesn't have padding properties.
                 child:  Padding(
                   padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.email,
-                        size: 30.0,
-                        color: Colors.teal.shade900,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 30.0,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
                         'example@gmail.com',
                         style: TextStyle(
                           color: Colors.teal.shade900,
-                          fontSize: 25.0,
+                          fontSize: 24.5,
                           fontFamily: 'SourceSansPro-Bold',
                           fontStyle: FontStyle.italic,
 
                         ),
-                      ),
-
-                    ],
-                  ),
+                  )
                 ),
 
 
               )
 
-          ],
+              )
+            ],
         ),
        ),
       ),
